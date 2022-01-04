3adb08398fe368e79825f9e8c3107fd0c717a6e6
Igor Shalyminov <ishalyminov@gmail.com>
2022-01-04 20:24:39 +0000

    CommonVoice processing script for Wav2Vec added

---
+++ examples/wav2vec/process_commonvoice.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import argparse
+
+import datasets
+from datasets import Audio
+import torchaudio
+from torch import Tensor
+
+
+def save_to_wav(item, output_folder):
+    filename_wav = os.path.splitext(item['path'])[0] + '.wav'
+    torchaudio.save(os.path.join(output_folder, filename_wav), Tensor([item['audio']['array']]), item['audio']['sampling_rate'])
+
+
+def main(locale, split, output_folder):
+    dataset = datasets.load_dataset('common_voice', locale, split=split)
+    dataset = dataset.cast_column("audio", Audio(sampling_rate=16_000))
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    dataset.map(lambda x: save_to_wav(x, output_folder))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('locale')
+    parser.add_argument('split')
+    parser.add_argument('output_folder')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args.locale, args.split, args.output_folder)
+
